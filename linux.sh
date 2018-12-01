0d90978adf70f74f47587be234f089638bf5e4c6
Marck <marck_tomack@protonmail.com>
2018-12-01 13:55:24 +0100

    improvements installation script

--- linux.sh
+++ linux.sh
@@ -4,13 +4,9 @@ BLUE='\033[1;34m'
 
 
 echo -e $RED Installing required python modules
-sudo pip install json
-sudo pip install sys
-sudo pip install time
-sudo pip install argparse
-sudo pip install requests
+pip install requests
 clear
-sleep 2 
+sleep 2
 sudo chmod +x search4.py
-sudo cp search4.py /user/bin/search4
+sudo cp search4.py /usr/bin/search4
 echo -e $YELLOW use search4 coomand on the terminal to use the tool.
