3faa603081282fe22b09411ed0990b3ef919340f
Anuj singh <36106218+7rillionaire@users.noreply.github.com>
2018-12-01 17:23:46 +0530

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,1 +1,15 @@
 
+echo Installing requirements ...
+
+apt update -y
+apt upgrade -y
+pkg install python -y
+pip install --upgrade pip
+pip install requests
+clear
+
+cd $HOME/Search4
+
+chmod +x search4.py
+
+echo use python search4.py to run the script.
